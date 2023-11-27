ed5cb2d45189a4d83e4aabbf78700fc0256a126f
Péter Palotai <peter.palotai@stud.uni-corvinus.hu>
2023-11-27 14:17:11 +0100

    geek html

--- WebAPIBase/wwwroot/scripts/geek.js
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -1,1 +1,31 @@
-﻿
+﻿window.onload = function () {
+    letöltés();
+}
+
+var viccek;
+
+
+function letöltés() { 
+
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+
+}
+
+function letöltésBefejeződött(d) {
+    let ide = document.getElementById("ide");
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        console.log(viccek[i].text)
+        let elem = document.createElement("li")
+        elem.innerHTML = viccek[i].text
+        ide.appendChild(elem)
+    }
+}
+
+
